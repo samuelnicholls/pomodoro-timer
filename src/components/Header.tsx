d62c6fa77634def9ce4ebb0643b5af7911ce2840
Samuel Nicholls <samuel.nicholls@hey.com>
2024-02-19 09:34:31 +0000

    Adding in header, footer and main content body

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Header: FC = () => {
+  return (
+    <header className="w-full md:p-6 p-4 bg-gray-800 border-b border-gray-200 text-white text-center">
+      <h1>Pomodoro Timer</h1>
+    </header>
+  )
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto">
-      <h1>Pomodoro Timer</h1>
+    <div className="flex min-h-screen flex-col items-center">
+      <Header />
+      <main className='flex w-full flex-1 flex-col py-8 lg:py-20 text-center bg-gray-800'>
+        <div>
+          <span>00:00</span>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 };
